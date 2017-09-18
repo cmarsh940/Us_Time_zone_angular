471efce62e283d4961096545cf2ea3a3ca85423d
cam <cam.marshall940@yahoo.com>
2017-09-18 08:35:08 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+//-.-. .- -- . .-. --- -. / -- .- .-. ... .... .- .-.. .-..//
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//-.-. .- -- . .-. --- -. / -- .- .-. ... .... .- .-.. .-..//
+export class AppComponent {
+  title = 'US Time Zone';
+  lastTimeZone = null;
+  time = new Date();
+  changeTime(timezone){
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    } else if (timezone === 'PST') {
+      this.time.setHours(this.time.getHours());
+    }
+    //-.-. .- -- . .-. --- -. / -- .- .-. ... .... .- .-.. .-..//
+    this.lastTimeZone = timezone;
+  }
+}
